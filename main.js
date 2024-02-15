a211df5a5f33b9575e3adec26ef5c7c4d082954c
ufokorea <seetheblue@naver.com>
2024-02-15 12:39:30 +0900

    final

--- main.js
+++ main.js
@@ -33,18 +33,21 @@ function puttask() {
 
     let item = taskitem.value;
     const color = document.getElementById("task-color").value;
-    let tasklist = {
-        id : Math.random().toString(36).substring(2, 18),
-        item : item,
-        isComplete: false,
-        color: color 
+
+    if(item != "") {        
+        let tasklist = {
+            id : Math.random().toString(36).substring(2, 18),
+            item : item,
+            isComplete: false,
+            color: color 
+        }
+
+        itemlist.push(tasklist)
+
+        document.getElementById("task-item").value = null;
+        
+        render();
     }
-
-    itemlist.push(tasklist)
-
-    document.getElementById("task-item").value = null;
-    
-    render();
 }
 
 function render() {
@@ -60,15 +63,9 @@ function render() {
     let putHtml='';
     for(let i=0; i < list.length; i++){
         let taskStyle = `style="background-color: ${list[i].color};"`;
-        if(list[i].isComplete === false) {
-            putHtml += `<div class="task-itemfalse" ${taskStyle}><div>${list[i].item}</div><div>
-            <button onclick="checkitem('${list[i].id}')"><i class="fa-solid fa-check"></i></button>
-            <button onclick="deleitem('${list[i].id}')"><i class="fa-solid fa-trash"></i></button></div></div>`;
-        } else if(list[i].isComplete === true) {
-            putHtml += `<div class="task-itemtrue"><div>${list[i].item}</div><div>
-            <button onclick="checkitem('${list[i].id}')"><i class="fa-solid fa-rotate-left"></i></button>
+            putHtml += `<div class="task-item${list[i].isComplete}"><div>${list[i].item}</div><div>
+            <button onclick="checkitem('${list[i].id}')">${list[i].isComplete ? "<i class='fa-solid fa-rotate-left'></i>" :"<i class='fa-solid fa-check'></i>"}</button>
             <button onclick="deleitem('${list[i].id}')"><i class="fa-solid fa-trash"></i></button></div></div>`;            
-        }
     }
 
     document.getElementById("putitem").innerHTML=putHtml;    
