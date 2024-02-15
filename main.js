c626915ac47b6244953f566e2b57f2668f830a3c
ufokorea <seetheblue@naver.com>
2024-02-15 12:47:46 +0900

    final

--- main.js
+++ main.js
@@ -63,18 +63,16 @@ function render() {
     let putHtml='';
     for(let i=0; i < list.length; i++){
         let taskStyle = `style="background-color: ${list[i].color};"`;
-if(list[i].isComplete === false) {
+        if(list[i].isComplete === false) {
             putHtml += `<div class="task-itemfalse" ${taskStyle}><div>${list[i].item}</div><div>
             <button onclick="checkitem('${list[i].id}')"><i class="fa-solid fa-check"></i></button>
             <button onclick="deleitem('${list[i].id}')"><i class="fa-solid fa-trash"></i></button></div></div>`;
         } else if(list[i].isComplete === true) {
             putHtml += `<div class="task-itemtrue"><div>${list[i].item}</div><div>
             <button onclick="checkitem('${list[i].id}')"><i class="fa-solid fa-rotate-left"></i></button>
-            <button onclick="deleitem('${list[i].id}')"><i class="fa-solid fa-trash"></i></button></div></div>`;             
+            <button onclick="deleitem('${list[i].id}')"><i class="fa-solid fa-trash"></i></button></div></div>`;            
+        }
     }
-
-    document.getElementById("putitem").innerHTML=putHtml;    
-}
 
 function checkitem(chkid) {
  
